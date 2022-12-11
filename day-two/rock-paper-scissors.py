c40eecaf050e422b7f7b5f70aa7121e223758877
Eduardo Lima <eduardo.fernandodelima@gmail.com>
2022-12-11 13:26:59 -0300

    day two part one

---
+++ day-two/rock-paper-scissors.py
@@ -0,0 +1,50 @@
+# Rock - 1 point
+# Paper - 2 points
+# Scissors - 3 points
+
+# Win - 6 points
+# Draw - 3 points
+# Lost - 0 points
+
+# A - X - Rock
+# B - Y - Paper
+# C - Z - Scissors
+
+# for each line
+#   check if I win, draw or lost
+#   check which move i made
+#   sum points
+
+file = open("C:\Dev\Advent-of-Code-22\day-two\input.txt", "r")
+
+strategy_guide = {
+    "X" : "C",
+    "Y" : "A",
+    "Z" : "B"
+}
+
+points_per_move = {
+    "X": 1,
+    "Y": 2,
+    "Z": 3,
+}
+
+equivalents = {
+    "A": "X",
+    "B": "Y",
+    "C": "Z"
+}
+
+current_sum = 0
+
+for line in file:
+    play = line.replace("\n", "").split(" ")
+    current_sum += points_per_move[play[1]]
+    # If is a victory
+    if strategy_guide[play[1]] == play[0]:
+        current_sum += 6
+    # elif is a draw
+    elif equivalents[play[0]] == play[1]:
+        current_sum += 3
+
+print(current_sum)
